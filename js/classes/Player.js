00f81d5402bdbdbc972a78bb083380c50b25d461
JohnathonHutt <johnathonhutt@gmail.com>
2023-01-23 21:38:50 -0500

    Fix collision blocks, wip implementing player collision detection

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,28 +1,48 @@
 class Player {
-  constructor(position) {
+  constructor({ position, collisionBlocks }) {
    this.position = position;
    this.velocity = {
       x: 0,
       y: 1,
-    }
+    };
+    this.width = 100;
     this.height = 100;
+    this.collisionBlocks = collisionBlocks;
   }
 
   draw() {
     c.fillStyle = 'red';
-    c.fillRect(this.position.x, this.position.y, 100, this.height);
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
     this.draw();
     
     this.position.x += this.velocity.x;
+    this.applyGravity();
+    this.checkForVerticalCollisions();
+  }
+
+  applyGravity() {
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y < canvas.height) {  
-     this.velocity.y += gravity;
+       this.velocity.y += gravity;
     } else {
       this.velocity.y = 0;
+    }
+  }
+
+  checkForVerticalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      // look for bottom of player and top of collision block
+      const collisionBlock = this.collisionBlocks[i];
+
+      if (collsision({ object1: this, object2: collisionBlock })) {
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+        }
+      }
     }
   }
 }
